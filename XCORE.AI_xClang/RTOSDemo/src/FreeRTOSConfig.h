1ba24b127f11544263209a65f49cffdd7e42f04a
chinglee-iot <61685396+chinglee-iot@users.noreply.github.com>
2023-10-11 16:13:00 +0800

    Update for SMP passive idle task (#13)

--- XCORE.AI_xClang/RTOSDemo/src/FreeRTOSConfig.h
+++ XCORE.AI_xClang/RTOSDemo/src/FreeRTOSConfig.h
@@ -43,7 +43,7 @@ your application. */
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     1
-#define configUSE_MINIMAL_IDLE_HOOK             1
+#define configUSE_PASSIVE_IDLE_HOOK             1
 #define configUSE_TICK_HOOK                     1
 #define configCHECK_FOR_STACK_OVERFLOW          0
 #define configUSE_MALLOC_FAILED_HOOK            1
