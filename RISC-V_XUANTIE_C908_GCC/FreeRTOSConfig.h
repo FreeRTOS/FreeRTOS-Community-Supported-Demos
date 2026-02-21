90e2b225a88ab584cd9253b02f5bb415b0a13815
Hongfei (Dylan) Poon <57466445+hongfei-poon@users.noreply.github.com>
2026-02-21 14:16:35 +0800

    format and clean

--- RISC-V_XUANTIE_C908_GCC/FreeRTOSConfig.h
+++ RISC-V_XUANTIE_C908_GCC/FreeRTOSConfig.h
@@ -1,4 +1,4 @@
- /*
+/*
  * Copyright (C) 2017-2024 Alibaba Group Holding Limited
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -20,123 +20,125 @@
 #define FREERTOS_CONFIG_H
 
 /*-----------------------------------------------------------
- * Application specific definitions.
- *
- * These definitions should be adjusted for your particular hardware and
- * application requirements.
- *
- * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
- *
- * See http://www.freertos.org/a00110.html.
- *----------------------------------------------------------*/
+* Application specific definitions.
+*
+* These definitions should be adjusted for your particular hardware and
+* application requirements.
+*
+* THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
+* FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
+*
+* See http://www.freertos.org/a00110.html.
+*----------------------------------------------------------*/
 
-#ifndef configNUMBER_OF_CORES
-    #define configNUMBER_OF_CORES                       1
-#endif
-
-#define configRUN_MULTIPLE_PRIORITIES               (1)
-#define configUSE_PASSIVE_IDLE_HOOK                 (0)
-#if configNUMBER_OF_CORES > 1
-    #define configUSE_CORE_AFFINITY                     1
+#if ( CONFIG_SMP )
+    #define configNUMBER_OF_CORES                  ( CONFIG_NR_CPUS )
 #else
-    #define configUSE_CORE_AFFINITY                     0
+    #define configNUMBER_OF_CORES                  ( 1 )
 #endif
 
-#define configCHECK_FOR_STACK_OVERFLOW              2
-#define configUSE_PREEMPTION                        1
-#define configUSE_IDLE_HOOK                         0
-#define configUSE_TICK_HOOK                         1
-#define configTASK_NOTIFICATION_ARRAY_ENTRIES       3
-#define configTICK_RATE_HZ                          (CONFIG_SYSTICK_HZ)
-#define configMINIMAL_STACK_SIZE                    (4096)
-#define configMAX_TASK_NAME_LEN                     (32)
-#define configUSE_TRACE_FACILITY                    (0)
-#define configUSE_16_BIT_TICKS                      (0)
-#define configIDLE_SHOULD_YIELD                     (1)
-#define configUSE_CO_ROUTINES                       (0)
-#define configUSE_MUTEXES                           (1)
-#define configUSE_RECURSIVE_MUTEXES                 (1)
-#define configQUEUE_REGISTRY_SIZE                   (10)
-#define configUSE_MALLOC_FAILED_HOOK                (1)
-#define configUSE_STATS_FORMATTING_FUNCTIONS        (1)
-#define configUSE_TIMERS                            (1)
-#define configTIMER_TASK_PRIORITY                   (1)
-#define configTIMER_QUEUE_LENGTH                    (128)
-#define configTIMER_TASK_STACK_DEPTH                (4096)
-#define configUSE_TIME_SLICING                      (1)
-#define configUSE_COUNTING_SEMAPHORES               (1)
-#define configNUM_THREAD_LOCAL_STORAGE_POINTERS     (5)
-#define configUSE_APPLICATION_TASK_TAG              (1)
-#define configUSE_TICKLESS_IDLE                     (1)
-#define configSUPPORT_STATIC_ALLOCATION             (1)
-#define configKERNEL_PROVIDED_STATIC_MEMORY         (1)
-#define configRECORD_STACK_HIGH_ADDRESS             (1)
-#define portCRITICAL_NESTING_IN_TCB                 (0)
-#define portHAS_STACK_OVERFLOW_CHECKING             (1)
-#define configGENERATE_RUN_TIME_STATS               (1)
-#define configMAX_PRIORITIES                        (10)
-#define configMAX_CO_ROUTINE_PRIORITIES             (2)
-#define configTOTAL_HEAP_SIZE                       (128 * 1024 * 1024)
-#define configUSE_QUEUE_SETS                        (1)
-#define INCLUDE_xTaskAbortDelay                     (1)
+#define configRUN_MULTIPLE_PRIORITIES              ( 1 )
+#define configUSE_PASSIVE_IDLE_HOOK                ( 0 )
+#if configNUMBER_OF_CORES > 1
+    #define configUSE_CORE_AFFINITY                1
+#else
+    #define configUSE_CORE_AFFINITY                0
+#endif
+
+#define configCHECK_FOR_STACK_OVERFLOW             2
+#define configUSE_PREEMPTION                       1
+#define configUSE_IDLE_HOOK                        0
+#define configUSE_TICK_HOOK                        1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES      3
+#define configTICK_RATE_HZ                         ( CONFIG_SYSTICK_HZ )
+#define configMINIMAL_STACK_SIZE                   ( 4096 )
+#define configMAX_TASK_NAME_LEN                    ( 32 )
+#define configUSE_TRACE_FACILITY                   ( 0 )
+#define configUSE_16_BIT_TICKS                     ( 0 )
+#define configIDLE_SHOULD_YIELD                    ( 1 )
+#define configUSE_CO_ROUTINES                      ( 0 )
+#define configUSE_MUTEXES                          ( 1 )
+#define configUSE_RECURSIVE_MUTEXES                ( 1 )
+#define configQUEUE_REGISTRY_SIZE                  ( 10 )
+#define configUSE_MALLOC_FAILED_HOOK               ( 1 )
+#define configUSE_STATS_FORMATTING_FUNCTIONS       ( 1 )
+#define configUSE_TIMERS                           ( 1 )
+#define configTIMER_TASK_PRIORITY                  ( 1 )
+#define configTIMER_QUEUE_LENGTH                   ( 128 )
+#define configTIMER_TASK_STACK_DEPTH               ( 4096 )
+#define configUSE_TIME_SLICING                     ( 1 )
+#define configUSE_COUNTING_SEMAPHORES              ( 1 )
+#define configNUM_THREAD_LOCAL_STORAGE_POINTERS    ( 5 )
+#define configUSE_APPLICATION_TASK_TAG             ( 1 )
+#define configUSE_TICKLESS_IDLE                    ( 1 )
+#define configSUPPORT_STATIC_ALLOCATION            ( 1 )
+#define configKERNEL_PROVIDED_STATIC_MEMORY        ( 1 )
+#define configRECORD_STACK_HIGH_ADDRESS            ( 1 )
+#define portCRITICAL_NESTING_IN_TCB                ( 0 )
+#define portHAS_STACK_OVERFLOW_CHECKING            ( 1 )
+#define configGENERATE_RUN_TIME_STATS              ( 1 )
+#define configMAX_PRIORITIES                       ( 10 )
+#define configMAX_CO_ROUTINE_PRIORITIES            ( 2 )
+#define configTOTAL_HEAP_SIZE                      ( 128 * 1024 * 1024 )
+#define configUSE_QUEUE_SETS                       ( 1 )
+#define INCLUDE_xTaskAbortDelay                    ( 1 )
 
 /* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet                    1
-#define INCLUDE_uxTaskPriorityGet                   1
-#define INCLUDE_vTaskDelete                         1
-#define INCLUDE_vTaskCleanUpResources               1
-#define INCLUDE_vTaskSuspend                        1
-#define INCLUDE_vTaskDelayUntil                     1
-#define INCLUDE_vTaskDelay                          1
-#define INCLUDE_uxTaskGetStackHighWaterMark         1
-#define INCLUDE_xTaskGetCurrentTaskHandle           1
-#define INCLUDE_xTaskGetSchedulerState              1
-#define INCLUDE_eTaskGetState                       1
-#define INCLUDE_xSemaphoreGetMutexHolder            1
-#define INCLUDE_xTimerPendFunctionCall              1
-#define INCLUDE_xTaskGetHandle                      1
+ * to exclude the API function. */
+#define INCLUDE_vTaskPrioritySet                   1
+#define INCLUDE_uxTaskPriorityGet                  1
+#define INCLUDE_vTaskDelete                        1
+#define INCLUDE_vTaskCleanUpResources              1
+#define INCLUDE_vTaskSuspend                       1
+#define INCLUDE_vTaskDelayUntil                    1
+#define INCLUDE_vTaskDelay                         1
+#define INCLUDE_uxTaskGetStackHighWaterMark        1
+#define INCLUDE_xTaskGetCurrentTaskHandle          1
+#define INCLUDE_xTaskGetSchedulerState             1
+#define INCLUDE_eTaskGetState                      1
+#define INCLUDE_xSemaphoreGetMutexHolder           1
+#define INCLUDE_xTimerPendFunctionCall             1
+#define INCLUDE_xTaskGetHandle                     1
 
 /* Priority 7, or 255 as only the top three bits are implemented.  This is the lowest priority. */
-#define configKERNEL_INTERRUPT_PRIORITY             (( unsigned char ) 7 << ( unsigned char ) 5 )
+#define configKERNEL_INTERRUPT_PRIORITY            ( ( unsigned char ) 7 << ( unsigned char ) 5 )
 /* Priority 5, or 160 as only the top three bits are implemented. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY        (( unsigned char ) 5 << ( unsigned char ) 5 )
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY       ( ( unsigned char ) 5 << ( unsigned char ) 5 )
 #define configASSERT( a )
-#define configPRINTF( X ) printf X
+#define configPRINTF( X )    printf X
 
 /**
  *  tests: run them one by one
  * */
-#define configSTART_TASK_NOTIFY_TESTS             1
-#define configSTART_TASK_NOTIFY_ARRAY_TESTS       0
-#define configSTART_BLOCKING_QUEUE_TESTS          0
-#define configSTART_SEMAPHORE_TESTS               0
-#define configSTART_POLLED_QUEUE_TESTS            0
-#define configSTART_INTEGER_MATH_TESTS            0
-#define configSTART_GENERIC_QUEUE_TESTS           0
-#define configSTART_PEEK_QUEUE_TESTS              0
-#define configSTART_MATH_TESTS                    0
-#define configSTART_RECURSIVE_MUTEX_TESTS         0
-#define configSTART_COUNTING_SEMAPHORE_TESTS      0
-#define configSTART_QUEUE_SET_TESTS               0
-#define configSTART_QUEUE_OVERWRITE_TESTS         0
-#define configSTART_EVENT_GROUP_TESTS             0
-#define configSTART_INTERRUPT_SEMAPHORE_TESTS     0
-#define configSTART_QUEUE_SET_POLLING_TESTS       0
-#define configSTART_BLOCK_TIME_TESTS              0
-#define configSTART_ABORT_DELAY_TESTS             0
-#define configSTART_MESSAGE_BUFFER_TESTS          0
-#define configSTART_STREAM_BUFFER_TESTS           0
-#define configSTART_STREAM_BUFFER_INTERRUPT_TESTS 0
-#define configSTART_TIMER_TESTS                   0
-#define configSTART_INTERRUPT_QUEUE_TESTS         0
-#define configSTART_DELETE_SELF_TESTS             0
-#define configSTART_REGISTER_TESTS                0
+#define configSTART_TASK_NOTIFY_TESTS                1
+#define configSTART_TASK_NOTIFY_ARRAY_TESTS          0
+#define configSTART_BLOCKING_QUEUE_TESTS             0
+#define configSTART_SEMAPHORE_TESTS                  0
+#define configSTART_POLLED_QUEUE_TESTS               0
+#define configSTART_INTEGER_MATH_TESTS               0
+#define configSTART_GENERIC_QUEUE_TESTS              0
+#define configSTART_PEEK_QUEUE_TESTS                 0
+#define configSTART_MATH_TESTS                       0
+#define configSTART_RECURSIVE_MUTEX_TESTS            0
+#define configSTART_COUNTING_SEMAPHORE_TESTS         0
+#define configSTART_QUEUE_SET_TESTS                  0
+#define configSTART_QUEUE_OVERWRITE_TESTS            0
+#define configSTART_EVENT_GROUP_TESTS                0
+#define configSTART_INTERRUPT_SEMAPHORE_TESTS        0
+#define configSTART_QUEUE_SET_POLLING_TESTS          0
+#define configSTART_BLOCK_TIME_TESTS                 0
+#define configSTART_ABORT_DELAY_TESTS                0
+#define configSTART_MESSAGE_BUFFER_TESTS             0
+#define configSTART_STREAM_BUFFER_TESTS              0
+#define configSTART_STREAM_BUFFER_INTERRUPT_TESTS    0
+#define configSTART_TIMER_TESTS                      0
+#define configSTART_INTERRUPT_QUEUE_TESTS            0
+#define configSTART_DELETE_SELF_TESTS                0
+#define configSTART_REGISTER_TESTS                   0
 
 #ifndef __ASSEMBLY__
-    extern volatile uint32_t g_irq_nested_level[configNUMBER_OF_CORES];
-    #define portASSERT_IF_IN_ISR()  configASSERT(g_irq_nested_level[portGET_CORE_ID()] == 0)
+    extern volatile uint32_t g_irq_nested_level[ configNUMBER_OF_CORES ];
+    #define portASSERT_IF_IN_ISR()    configASSERT( g_irq_nested_level[ portGET_CORE_ID() ] == 0 )
 #endif /* __ASSEMBLY__ */
 
 #endif /* FREERTOS_CONFIG_H */
